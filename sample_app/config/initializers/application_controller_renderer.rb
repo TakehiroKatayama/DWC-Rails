31ada212accb75d2c30d653d83f93bdb117d95e3
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-05 09:40:40 +0000

    [finish]2-7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f70b6baad8a4d5ac78d810c192a8d2d9afd579cf2f780f7bab15b7c9df2873cc1c355f217819eb445a1e2f2b45367cad7715c9bf35aa5da1659233fa85571570'
